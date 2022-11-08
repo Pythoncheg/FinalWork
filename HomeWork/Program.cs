fc02ddd419881894b998f4eb4a3de7ba630e3f83
Denis_Gerasimov <Pythonchic@gmail.com>
2022-11-08 21:42:57 +0300

    Описал метод сортировки массива

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -32,3 +32,16 @@ int FindLength(string[] array)
     }
     return count;
 }
+
+void FinalSort(string[] array1, string[] array2)
+{
+    int j = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3)
+        {
+            array2[j] = array1[i];
+            j++;
+        }
+    }
+}
