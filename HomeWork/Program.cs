8892b79be73ba146a1fc3ca00a20dfdebfc812d5
Denis_Gerasimov <Pythonchic@gmail.com>
2022-11-08 21:41:10 +0300

    Описал метод печати массива

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+void PrintArray(string[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == arr.Length || arr.Length <= 1)
+            Console.Write($"[{arr[i]}] ");
+        else if (i == 0)
+
+            Console.Write($"[{arr[i]}, ");
+
+        else if (i != arr.Length - 1)
+
+            Console.Write($"{arr[i]}, ");
+
+        else
+            Console.Write($"{arr[i]}] ");
+
+    }
+    Console.WriteLine();
+}
